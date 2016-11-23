c810c1a1121f2e8cfff36bf7726f124d06f63ac0
extiser <1sappho@gmail.com>
2016-11-23 14:02:47 +0500

    build

---
+++ tars-config.js
@@ -0,0 +1,62 @@
+module.exports = {
+    "postcss": [],
+    "svg": {
+        "active": true,
+        "workflow": "sprite",
+        "symbolsConfig": {
+            "loadingType": "inject",
+            "usePolyfillForExternalSymbols": true,
+            "pathToExternalSymbolsFile": ""
+        }
+    },
+    "css": {
+        "workflow": "concat"
+    },
+    "js": {
+        "workflow": "concat",
+        "bundler": "webpack",
+        "lint": false,
+        "useBabel": false,
+        "removeConsoleLog": true,
+        "webpack": {
+            "useHMR": false
+        },
+        "jsPathsToConcatBeforeModulesJs": [],
+        "lintJsCodeBeforeModules": false,
+        "jsPathsToConcatAfterModulesJs": [],
+        "lintJsCodeAfterModules": false
+    },
+    "sourcemaps": {
+        "js": {
+            "active": false,
+            "inline": true
+        },
+        "css": {
+            "active": false,
+            "inline": true
+        }
+    },
+    "notifyConfig": {
+        "useNotify": true,
+        "title": "TARS notification",
+        "sounds": {},
+        "taskFinishedText": "Task finished at: "
+    },
+    "minifyHtml": false,
+    "generateStaticPath": true,
+    "buildPath": "./builds/",
+    "useBuildVersioning": false,
+    "useArchiver": false,
+    "ulimit": 4096,
+    "templater": "pug",
+    "cssPreprocessor": "scss",
+    "useImagesForDisplayWithDpi": [
+        96
+    ],
+    "fs": {
+        "staticFolderName": "static",
+        "imagesFolderName": "images",
+        "componentsFolderName": "components"
+    },
+    "staticPrefix": "static/"
+};
